0aa6d2a0d5a9bbb67ac42935fe6c805c2693e1fa
Milgo21 <felixmilgo21@gmail.com>
2023-02-23 08:57:00 +0300

    Navigate

--- src/app/pagenotfound/pagenotfound.component.ts
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagenotfound',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagenotfound.component.css']
 })
 export class PagenotfoundComponent {
+
+  constructor( private route:Router, private activatedRoute:ActivatedRoute){}
+
+
+
+  navigateHome(){
+    this.route.navigate(['Home']);
+    // this.route.navigate(['Home'],{relativeTo:this.activatedRoute});
+    }
+
+
 
 }
